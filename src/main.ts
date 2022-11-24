4a3e777dc318c7c43fe9a19a4771933ea5cc428a
Bonilla Andres <mailofbonilla@gmail.com>
2022-11-24 09:35:07 -0500

    feat: new icons, and home view

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://www.google.com'],
+    methods: ['POST', 'PUT', 'DELETE', 'GET']
+  });
   await app.listen(4000);
 }
 bootstrap();
